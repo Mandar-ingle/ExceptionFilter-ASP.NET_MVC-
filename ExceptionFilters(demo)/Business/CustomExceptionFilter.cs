ad0fba73d28307a233e55e4eff4e40acf69aabb0
Administrator <Administrator@MANDAR>
2021-07-25 23:15:14 +0530

    Add project files.

---
+++ ExceptionFilters(demo)/Business/CustomExceptionFilter.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExceptionFilters_demo_.Business
+{
+    public class CustomExceptionFilter : IExceptionFilter
+    {
+        private readonly IModelMetadataProvider _modelMetadataProvider;
+        public CustomExceptionFilter(IModelMetadataProvider modelMetadataProvider)
+        {
+            _modelMetadataProvider = modelMetadataProvider;
+        }
+        public void OnException(ExceptionContext context)
+        {
+            var result = new ViewResult { ViewName = "CustomError" };
+            result.ViewData = new ViewDataDictionary(_modelMetadataProvider, context.ModelState);
+            result.ViewData.Add("Exception", context.Exception);
+            context.ExceptionHandled = true;
+            context.Result = result;
+            //throw new NotImplementedException();
+        }
+    }
+}
